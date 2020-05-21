fb81b735c790c2c6dfa5c4b8a4aea89786620764
Pavlo Zhukov <pahazhukov@gmail.com>
2020-05-22 01:34:36 +0300

    fix(mdc): add commonly used components

--- src/mdc/index.js
+++ src/mdc/index.js
@@ -1,2 +1,18 @@
 export {Button} from '@rmwc/button'
+export {
+  Card,
+  CardActionButton,
+  CardActionButtons,
+  CardActions,
+} from '@rmwc/card'
+export {
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  DialogButton,
+} from '@rmwc/dialog'
+export {Grid, GridCell} from '@rmwc/grid'
+export {LinearProgress} from '@rmwc/linear-progress'
+export {List, ListItem, ListItemMeta} from '@rmwc/list'
+export {Menu, MenuItem, MenuSurface, MenuSurfaceAnchor} from '@rmwc/menu'
 export {TextField} from '@rmwc/textfield'
