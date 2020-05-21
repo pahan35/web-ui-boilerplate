ceb9c5c7de082ddeba43519dc228d48d05f259d3
Pavlo Zhukov <pahazhukov@gmail.com>
2020-05-22 01:35:07 +0300

    fix(lint): remove unused var

--- src/Components/SomeComponent.js
+++ src/Components/SomeComponent.js
@@ -1,6 +1,6 @@
 import {Button} from 'mdc'
 import React from 'react'
 
-export default function SomeComponent(props) {
+export default function SomeComponent() {
   return <Button>I am SomeComponent 35</Button>
 }
